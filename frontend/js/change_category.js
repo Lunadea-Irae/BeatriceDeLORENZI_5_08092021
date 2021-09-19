52861a7b4eba97781dc468c586ed03826a83588d
Béatrice de LORENZI <lunadea.irae@gmail.com>
2021-09-19 07:24:03 +0200

    change original category

--- frontend/js/change_category.js
+++ frontend/js/change_category.js
@@ -1,7 +1,7 @@
 let configData =
 {
     //..........CHANGE CATEGORY HERE (avalable teddies and cameras for now)
-    'category': 'cameras',
+    'category': 'teddies',
     //.....................................................................
 
 
